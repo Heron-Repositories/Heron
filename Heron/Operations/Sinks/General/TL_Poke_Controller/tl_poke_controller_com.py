969083d20271a71c85e8ed61a6f722ec1cf74674
georgedimitriadis <gdimitri@hotmail.com>
2021-08-10 17:45:15 +0100

    Added Poke Controller Node for the Transfer Learning experiment

---
+++ Heron/Operations/Sinks/General/TL_Poke_Controller/tl_poke_controller_com.py
@@ -0,0 +1,31 @@
+
+import os
+from Heron import general_utils as gu
+Exec = os.path.realpath(__file__)
+
+
+# <editor-fold desc="The following code is called from the GUI process as part of the generation of the node.
+# It is meant to create node specific elements (not part of a generic node).
+# This is where a new node's individual elements should be defined">
+
+"""
+Properties of the generated Node
+"""
+BaseName = 'TL Poke Controller'
+NodeAttributeNames = ['Parameters', 'Start']
+NodeAttributeType = ['Static', 'Input']
+ParameterNames = ['Com port', 'Availability Time (s)', 'Availability Freq (Hz)', 'Success Freq (Hz)']
+ParameterTypes = ['str', 'float', 'list', 'list']
+ParametersDefaultValues = ['COM5', 40, [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000],
+                           [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]]
+WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'tl_poke_controller_worker.py')
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called as its own process when the editor starts the graph">
+if __name__ == "__main__":
+    tl_projector_output_com = gu.start_the_sink_communications_process()
+    gu.register_exit_signals(tl_projector_output_com.on_kill)
+    tl_projector_output_com.start_ioloop()
+
+# </editor-fold>
---
+++ Heron/Operations/Sinks/General/TL_Poke_Controller/tl_poke_controller_worker.py
@@ -0,0 +1,124 @@
+
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+
+import time
+import serial
+import threading
+from Heron.communication.socket_for_serialization import Socket
+from Heron import general_utils as gu
+
+need_parameters = True
+arduino_serial: serial.Serial
+avail_time: float
+avail_freq: int
+succ_freq: int
+availability_period_is_running = False
+
+
+def freq_to_signal(freq):
+    freq = int(freq)
+    if freq < 500:
+        freq = 500
+    if freq > 5000:
+        freq = 5000
+    return chr(int(freq/500 + 97)).encode('utf-8')
+
+
+def initialise(_worker_object):
+    global arduino_serial
+    global avail_time
+    global avail_freq
+    global succ_freq
+
+    need_parameters = True
+
+    while need_parameters:
+        try:
+            parameters = _worker_object.parameters
+            com_port = parameters[0]
+            avail_time = parameters[1]
+            avail_freq = parameters[2]
+            succ_freq = parameters[3]
+            need_parameters = False
+        except:
+            pass
+
+        try:
+            arduino_serial = serial.Serial(com_port)
+        except Exception as e:
+            print(e)
+
+
+def start_availability_thread():
+    global arduino_serial
+    global availability_period_is_running
+    global avail_time
+    global avail_freq
+    global succ_freq
+
+    sleep_dt = 0.18
+    total_steps = int(avail_time / sleep_dt)
+
+    availability_period_is_running = True
+    arduino_serial.reset_input_buffer()
+    step = 0
+    while availability_period_is_running:
+
+        bytes_in_buffer = arduino_serial.in_waiting
+        string_in = arduino_serial.read(bytes_in_buffer).decode('utf-8')
+
+        if bytes_in_buffer:
+            if '0\r\n' in string_in:
+                #print('Play success sound and give food')
+                availability_period_is_running = False
+                try:
+                    arduino_serial.write('a'.encode('utf-8'))
+                    arduino_serial.write(freq_to_signal(succ_freq))
+                    time.sleep(0.6)
+                    arduino_serial.write(freq_to_signal(succ_freq))
+                except Exception as e:
+                    print(e)
+        elif step >= total_steps:
+            #print('Finish')
+            availability_period_is_running = False
+        else:
+            #print('Play Availability sound')
+            try:
+                arduino_serial.write(freq_to_signal(avail_freq))
+                print(arduino_serial.read(arduino_serial.in_waiting))
+            except Exception as e:
+                print(e)
+            time.sleep(sleep_dt)
+        step += 1
+
+
+def start_availability_period(data, parameters):
+    global availability_period_is_running
+
+    topic = data[0].decode('utf-8')
+    message = Socket.reconstruct_array_from_bytes_message(data[1:])
+
+    if not availability_period_is_running:
+        try:
+            avail_thread = threading.Thread(target=start_availability_thread)
+            avail_thread.start()
+        except Exception as e:
+            print(e)
+
+
+def on_end_of_life():
+    global arduino_serial
+    arduino_serial.close()
+
+
+if __name__ == "__main__":
+    worker_object = gu.start_the_sink_worker_process(work_function=start_availability_period,
+                                                     end_of_life_function=on_end_of_life,
+                                                     initialisation_function=initialise)
+    worker_object.start_ioloop()
