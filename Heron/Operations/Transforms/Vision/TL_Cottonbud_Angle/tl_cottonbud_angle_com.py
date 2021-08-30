b6e2acaa70136ddc105580971d3276f0fec237ba
georgedimitriadis <gdimitri@hotmail.com>
2021-08-30 19:16:27 +0100

    Added Cottonbud angle detector for Transfer Learning Experiment

---
+++ Heron/Operations/Transforms/Vision/TL_Cottonbud_Angle/tl_cottonbud_angle_com.py
@@ -0,0 +1,33 @@
+
+import os
+from Heron import general_utils as gu
+Exec = os.path.realpath(__file__)
+
+
+# <editor-fold desc="The following code is called from the GUI process as part of the generation of the node.
+# It is meant to create node specific elements (not part of a generic node).
+# This is where a new node's individual elements should be defined">
+
+"""
+Properties of the generated Node
+"""
+BaseName = 'TL Cottonbud Angle'
+NodeAttributeNames = ['Parameters', 'Frame In', 'Angle Out', 'Image Out']
+NodeAttributeType = ['Static', 'Input', 'Output', 'Output']
+ParameterNames = ['Model zoo yaml', 'Model Weights File', 'Number of Classes']
+ParameterTypes = ['str', 'str', 'int']
+ParametersDefaultValues = ['model_zoo_file.yaml', 'model_final.pth', 5]
+WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'tl_cottonbud_angle_worker.py')
+
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called as its own process when the editor starts the graph">
+if __name__ == "__main__":
+    # Since this is a multiple output node we need to pass NodeAttributeType, NodeAttributeNames to
+    # general_utilities start_the_xxx_communications_process function
+    tl_cottonbud_angle_com = gu.start_the_transform_communications_process(NodeAttributeType, NodeAttributeNames)
+    gu.register_exit_signals(tl_cottonbud_angle_com.on_kill)
+    tl_cottonbud_angle_com.start_ioloop()
+
+# </editor-fold>
---
+++ Heron/Operations/Transforms/Vision/TL_Cottonbud_Angle/tl_cottonbud_angle_worker.py
@@ -0,0 +1,143 @@
+
+import sys
+from os import path
+import cv2
+import numpy as np
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+
+import logging
+import torch, detectron2
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+
+from Heron.communication.socket_for_serialization import Socket
+from Heron import general_utils as gu
+from Heron.Operations.Transforms.Vision.TL_Cottonbud_Angle import tl_cottonbud_angle_com
+from Heron.communication.transform_worker import TransformWorker
+
+
+worker_object: TransformWorker
+predictor = None
+
+handler = logging.FileHandler('/mnt/e/temp/detect.log')
+logger = logging.getLogger('Hello')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+
+def visualise_prediction(image, outputs):
+    v = Visualizer(image[:, :, ::-1], scale=1.0)
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    im = v.get_image()[:, :, ::-1].astype(np.uint8)
+
+    return im
+
+
+def get_parameters(worker_object):
+    global predictor
+    global logger
+
+    logger.debug(predictor)
+    # The default value of the 1st parameter is a generic name that must always be changed. That is why the below is
+    # a valid test of whether the parameters have been read
+    if tl_cottonbud_angle_com.ParametersDefaultValues[0] == worker_object.parameters[0]:
+        return False
+
+    if predictor is None:
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file(worker_object.parameters[0]))
+        cfg.MODEL.WEIGHTS = worker_object.parameters[1]
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = worker_object.parameters[2]
+        predictor = DefaultPredictor(cfg)
+
+    return True
+
+
+def calculate_stick_angle(outputs):
+
+    instances = outputs['instances']
+    center_of_bud_1 = None
+    center_of_bud_2 = None
+    angle = None
+
+    if len(instances) > 1:
+        for i in range(len(instances)):
+            instance = instances[i]
+            if instance.pred_classes == 3 and instance.scores > 0.95:
+                bud_box = instance.pred_boxes
+                center_of_bud = bud_box.get_centers()
+                if center_of_bud_1 is None:
+                    center_of_bud_1 = center_of_bud.flatten().flatten()
+                else:
+                    center_of_bud_2 = center_of_bud.flatten().flatten()
+
+        if center_of_bud_1 is not None and center_of_bud_2 is not None:
+            if center_of_bud_1[0] > center_of_bud_2[0]:
+                zeroed_tensor = torch.subtract(center_of_bud_1, center_of_bud_2)
+            else:
+                zeroed_tensor = torch.subtract(center_of_bud_2, center_of_bud_1)
+            zeroed_tensor = zeroed_tensor.to('cpu')
+            cos = zeroed_tensor[0]/(zeroed_tensor[0].pow(2) + zeroed_tensor[1].pow(2)).pow(0.5)
+            angle = torch.rad2deg(torch.acos(cos))
+
+            if zeroed_tensor[1] > 0:
+                angle = -angle
+
+    return angle
+
+
+def detect(data, parameters):
+    global logger
+    global worker_object
+    global predictor
+
+    image_with_predictions = np.array(np.random.random((100, 100)))
+    logger.debug(1)
+
+    if data is None or parameters is None:
+        worker_object.worker_visualisable_result = np.array(['Not Calculated'])
+    else:
+        message = data[1:]  # data[0] is the topic
+        image = Socket.reconstruct_array_from_bytes_message_cv2correction(message)
+        image = cv2.resize(image, (200, 204))
+        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+
+        try:
+            outputs = predictor(image)
+            angle = calculate_stick_angle(outputs)
+            if angle is not None:
+                angle = np.array([angle.tolist()])
+                worker_object.worker_visualisable_result = angle
+                image_with_predictions = np.ascontiguousarray(visualise_prediction(image, outputs))
+            else:
+                worker_object.worker_visualisable_result = np.array([None])
+
+            logger.debug('Angle = {}'.format(worker_object.worker_visualisable_result))
+            logger.debug('Pred Image shape = {}'.format(image_with_predictions.shape))
+
+        except Exception as e:
+            worker_object.worker_visualisable_result = np.array([None])
+            logger.debug('Detecting cottonbud angle {} operation failed'.format(worker_object.node_index))
+            logger.debug(e)
+
+    # The order in the list should be the same as the order of outputs shown on the Node from top to bottom
+    return [worker_object.worker_visualisable_result, image_with_predictions]
+
+
+def on_end_of_life():
+    global worker_object
+    global predictor
+
+    del worker_object
+    del predictor
+
+
+if __name__ == "__main__":
+    worker_object = gu.start_the_transform_worker_process(detect, on_end_of_life, get_parameters)
+    worker_object.start_ioloop()
