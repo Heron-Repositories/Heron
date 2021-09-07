1423d01d25960db9cc184dd46288dd88e7cd8b4f
georgedimitriadis <gdimitri@hotmail.com>
2021-09-07 13:13:58 +0100

    Fixing a bug in NearZero_Controller Node

--- Heron/Operations/Sinks/Motion/NearZero_Controller/near_zero_controller_worker.py
+++ Heron/Operations/Sinks/Motion/NearZero_Controller/near_zero_controller_worker.py
@@ -15,12 +15,7 @@ from Heron.Operations.Sinks.Motion.NearZero_Controller import near_zero_controll
 
 worker_object: SinkWorker
 need_parameters = True
-use_pylibi2c = False
 motor_controller = None
-motor = None
-pos_or_rot = None
-value = None
-current = None
 
 
 def initialise_near_zero_controller(i2c_address):
@@ -34,16 +29,9 @@ def initialise_near_zero_controller(i2c_address):
 
 
 def move_motor(data, parameters):
-    global worker_object
     global need_parameters
-    global use_pylibi2c
     global motor_controller
-    global motor
-    global pos_or_rot
-    global value
-    global current
 
-    start = time.perf_counter()
     try:
         use_pylibi2c = parameters[0]
         i2c_address = int(parameters[1], base=16)
@@ -60,7 +48,7 @@ def move_motor(data, parameters):
         current = near_zero_controller_com.ParametersDefaultValues[5]
     if pos_or_rot == 'r':
         pos_or_rot = 'v'
-    if use_pylibi2c:
+    if use_pylibi2c and motor_controller is None:
         motor_controller = initialise_near_zero_controller(i2c_address)
 
     message = data[1:]  # data[0] is the topic
@@ -69,10 +57,8 @@ def move_motor(data, parameters):
         command = '{}{}{}c{}'.format(motor, pos_or_rot, value, current)
         if use_pylibi2c:
             motor_controller.write(0x0, command)
-        print('Moving controller {} with command {}'.format(worker_object.node_index, command))
-
-    end = time.perf_counter()
-    #print('ooooTime of frame receive = {}, saved = {}, dif = {}'.format(start, end, end-start))
+        else:
+            print('Moving controller {} with command {}'.format(worker_object.node_index, command))
 
 
 def on_end_of_life():
