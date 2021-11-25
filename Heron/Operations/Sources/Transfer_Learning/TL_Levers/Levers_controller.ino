099fb313b824b50f1cebc415c95559a27ce45447
georgedimitriadis <gdimitri@hotmail.com>
2021-11-25 15:27:54 +0000

    Added the arduino .ino file to the TL Levers folder

---
+++ Heron/Operations/Sources/Transfer_Learning/TL_Levers/Levers_controller.ino
@@ -0,0 +1,76 @@
+
+#define beambreak_in 4
+#define left_lever 5
+#define right_lever 7
+
+int beambreak_value = 0;
+int left_lever_value = 0;
+int right_lever_value = 0;
+
+unsigned long currentTime = 0;
+unsigned long leftLeverStartTime = 0;
+unsigned long rightLeverStartTime = 0;
+unsigned long leftLeverPressedTime = 0;
+unsigned long rightLeverPressedTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(left_lever, INPUT);
+  pinMode(right_lever, INPUT);
+  pinMode(beambreak_in, INPUT_PULLUP);
+}
+
+void loop() {
+  
+  beambreak_value = digitalRead(beambreak_in);
+  if (beambreak_value == 0)
+  {
+    left_lever_value = digitalRead(left_lever);
+    right_lever_value = digitalRead(right_lever);
+
+    if (right_lever_value == 1 && left_lever_value == 1){
+      leftLeverStartTime = 0;
+      rightLeverStartTime = 0;
+      leftLeverPressedTime = 0;
+      rightLeverPressedTime = 0;
+    }else{
+      if (left_lever_value == 1){
+      if (leftLeverStartTime == 0){
+        leftLeverStartTime = millis();
+      }
+      leftLeverPressedTime = millis() - leftLeverStartTime;
+    }else{
+      leftLeverStartTime = 0;
+      leftLeverPressedTime = 0;
+    }
+
+    if (right_lever_value == 1){
+      if (rightLeverStartTime == 0){
+        rightLeverStartTime = millis();
+      }
+      rightLeverPressedTime = millis() - rightLeverStartTime;
+    }else{
+        rightLeverStartTime =0;
+        rightLeverPressedTime = 0;
+      }
+    }
+  }
+  
+  if (beambreak_value == 1){
+    leftLeverStartTime = 0;
+    rightLeverStartTime = 0;
+    leftLeverPressedTime = 0;
+    rightLeverPressedTime = 0;
+  }
+
+  String string_out = "Poke=";
+    string_out.concat(beambreak_value);
+    string_out.concat("#Left=");
+    string_out.concat(leftLeverPressedTime);
+    string_out.concat("#Right=");
+    string_out.concat(rightLeverPressedTime);
+    Serial.println(string_out);
+
+  delay(100);
+  
+}
