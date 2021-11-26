ef71f9005e2ef5f091885742a61845e2e6f8d325
georgedimitriadis <gdimitri@hotmail.com>
2021-11-26 17:10:03 +0000

    Created a Visualisation class that takes over in Node visualisation (not needed to be done through the worker object anymore)

--- Heron/gui/visualisation.py
+++ Heron/gui/visualisation.py
@@ -3,6 +3,7 @@ import cv2
 import threading
 
 import numpy as np
+from Heron import general_utils as gu
 
 
 class Visualisation():
@@ -11,8 +12,11 @@ class Visualisation():
         self.node_index = node_index
         self.visualisation_on = False
         self.visualisation_thread = None
-        self.visualised_data: np.ndarray
+        self.checking_thread: threading.thread
+        self.visualised_data = np.empty((100, 100))
         self.running = True
+        self.window_showing = False
+        self.set_new_visualisation_loop(self.visualisation_loop)
 
     def set_new_visualisation_loop(self, new_visualisation_loop):
         """
@@ -23,27 +27,27 @@ class Visualisation():
         """
         self.visualisation_loop = new_visualisation_loop
 
-    def visualisation_loop(self):
+    @staticmethod
+    def visualisation_loop(vis_object):
         """
         When the visualisation parameter in a node is set to True then this loop starts in a new visualisation thread.
-        The thread terminates when the visualisation_on boolean is turned off
+        The thread terminates when the visualisation_on boolean is turned off. Because this is a static method it can
+        be overridden by another loop from an xxx_worker script using the set_new_visualisation_loop
         :return: Nothing
         """
-        window_showing = False
-
-        while self.running:
-            while self.visualisation_on:
-                if not window_showing:
-                    window_name = '{} {}'.format(self.node_name, self.node_index)
+        while vis_object.running:
+            while vis_object.visualisation_on:
+                if not vis_object.window_showing:
+                    window_name = '{} {}'.format(vis_object.node_name, vis_object.node_index)
                     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-                    cv2.imshow(window_name, self.visualised_data)
+                    cv2.imshow(window_name, vis_object.visualised_data)
                     cv2.waitKey(1)
-                    window_showing = True
-                if window_showing:
+                    vis_object.window_showing = True
+                if vis_object.window_showing:
                     try:
                         width = cv2.getWindowImageRect(window_name)[2]
                         height = cv2.getWindowImageRect(window_name)[3]
-                        image = cv2.resize(self.visualised_data, (width, height), interpolation=cv2.INTER_AREA)
+                        image = cv2.resize(vis_object.visualised_data, (width, height), interpolation=cv2.INTER_AREA)
                         cv2.imshow(window_name, image)
                         cv2.waitKey(1)
                     except Exception as e:
@@ -51,24 +55,31 @@ class Visualisation():
 
             cv2.destroyAllWindows()
             cv2.waitKey(1)
-            window_showing = False
-        print('hello')
+            vis_object.window_showing = False
 
     def visualisation_loop_update(self):
         """
         The function that is run at every cycle of the WORKER_FUNCTION to check if the visualisation_on bool is True
         for the first time. When that happens it starts the visualisation loop. The loop takes care of the showing
-        and hiding of the visualisation window
+        and hiding of the visualisation window.
         :return: Nothing
         """
-        if self.visualisation_on and self.visualisation_thread is None:
-            self.visualisation_thread = threading.Thread(target=self.visualisation_loop, daemon=True)
+        while not self.visualisation_on:
+            gu.accurate_delay(50)
+
+        if self.visualisation_thread is None:
+            self.visualisation_thread = threading.Thread(target=self.visualisation_loop, daemon=True, args=(self,))
             self.visualisation_on = True
             self.visualisation_thread.start()
 
+    def visualisation_init(self):
+        self.checking_thread = threading.Thread(target=self.visualisation_loop_update)
+        self.checking_thread.start()
+
     def kill(self):
+        self.window_showing = False
         if self.visualisation_on:
             self.visualisation_on = False
-            cv2.waitKey(10)
         self.running = False
-        cv2.waitKey(10)
+        gu.accurate_delay(200)
+
