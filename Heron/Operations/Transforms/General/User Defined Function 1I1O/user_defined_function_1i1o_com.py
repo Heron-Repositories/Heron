c805cfd498453fc2f358c68fd54e6f6686d746da
georgedimitriadis <gdimitri@hotmail.com>
2022-01-24 17:36:43 +0000

    Created a one Input one Output user defined Operation

---
+++ Heron/Operations/Transforms/General/User Defined Function 1I1O/user_defined_function_1i1o_com.py
@@ -0,0 +1,49 @@
+# This is the code that defines the Parameters, Inputs and Outputs of the Node. Most of the code should not be
+# changed. The only parts that should be changed are the information in the lists of the properties of the Node and the
+# name of the xxx_com variable under the if __name__ == "__main__": clause
+
+
+# <editor-fold desc="The following 9 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import os
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+
+from Heron import general_utils as gu
+Exec = os.path.realpath(__file__)
+# </editor-fold>
+
+# <editor-fold desc="The following code is called from the GUI process as part of the generation of the node.
+# It is meant to create node specific elements (not part of a generic node).
+# This is where a new node's individual elements should be defined">
+"""
+Properties of the generated Node
+"""
+BaseName = 'User Defined Function 1I 1O'
+NodeAttributeNames = ['Parameters', 'Input', 'Output']
+
+NodeAttributeType = ['Static', 'Input', 'Output']
+
+ParameterNames = [r"Full path to function's filename"]
+
+ParameterTypes = ['str']
+
+ParametersDefaultValues = [r'E:\Temp\function.py']
+
+WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'user_defined_function_1i1o_worker.py')
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called as its own process when the editor starts the graph.
+#  You can refactor the name of the xxx_com variable but do not change anything else">
+if __name__ == "__main__":
+    transform_template_com = gu.start_the_transform_communications_process(NodeAttributeType, NodeAttributeNames)
+    gu.register_exit_signals(transform_template_com.on_kill)
+    transform_template_com.start_ioloop()
+
+# </editor-fold>
---
+++ Heron/Operations/Transforms/General/User Defined Function 1I1O/user_defined_function_1i1o_worker.py
@@ -0,0 +1,85 @@
+
+# This is the code of the worker part of the Transform Operation. Here the user needs to write most of the code in
+# order to define the Operation's functionality.
+
+# <editor-fold desc="The following 6 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import sys
+from os import path
+import numpy as np
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+# </editor-fold>
+
+# <editor-fold desc="Extra imports if required">
+import importlib as ilb
+from Heron.communication.socket_for_serialization import Socket
+from Heron import general_utils as gu, constants as ct
+# </editor-fold>
+
+# <editor-fold desc="Global variables if required. Global variables operate obviously within the scope of the process
+# that is running when this script is called so they pose no existential threats and are very useful in keeping state
+# over different calls of the work function (see below).">
+need_parameters = True
+functions_path: str
+function = None
+# </editor-fold>
+
+
+def initialise(worker_object):
+    global need_parameters
+    global functions_path
+    global function
+
+    try:
+        parameters = worker_object.parameters
+        functions_path = parameters[0]
+        need_parameters = False
+
+        functions_dir = path.dirname(path.abspath(functions_path))
+        functions_name = path.basename(path.abspath(functions_path)).split('.')[0]
+        sys.path.insert(0, functions_dir)
+
+        module = ilb.import_module(functions_name)
+        function = getattr(module, 'function')
+    except:
+        return False
+
+    return True
+
+
+def work_function(data, parameters):
+    global function
+
+    topic = data[0]
+
+    message = data[1:]  # data[0] is the topic
+    input_data = Socket.reconstruct_array_from_bytes_message(message)
+
+    result = function(input_data)
+
+    if type(result) == np.ndarray:
+        result = [result]
+    elif type(result) == list:
+        if type(result[0]) == np.ndarray:
+            return result
+        else:
+            result = [np.array(result)]
+    else:
+        result = [np.array([result])]
+
+    return result
+
+
+def on_end_of_life():
+    pass
+
+
+if __name__ == "__main__":
+    worker_object = gu.start_the_transform_worker_process(work_function=work_function,
+                                                          end_of_life_function=on_end_of_life,
+                                                          initialisation_function=initialise)
+    worker_object.start_ioloop()
