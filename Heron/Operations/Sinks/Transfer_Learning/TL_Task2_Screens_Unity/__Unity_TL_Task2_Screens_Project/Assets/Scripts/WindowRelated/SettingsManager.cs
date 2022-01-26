8b7bc5226705bfd81d08c25ad56d1ae906f72248
georgedimitriadis <gdimitri@hotmail.com>
2022-01-26 11:01:50 +0000

    Added SettingsManager to Unity project to allow window positioning

---
+++ Heron/Operations/Sinks/Transfer_Learning/TL_Task2_Screens_Unity/__Unity_TL_Task2_Screens_Project/Assets/Scripts/WindowRelated/SettingsManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_STANDALONE_WIN
+ 
+using System;
+using System.Runtime.InteropServices;
+ 
+#endif
+
+public class SettingsManager : MonoBehaviour
+{
+
+    public bool fullScreen = false;
+    private static string fullScreenKey = "Full Screen";
+    public int minDisplaySize = 384;
+    private static int monitorWidth;
+    private static int monitorHeight;
+    public int windowPositionX = 1000;
+    public int windowPositionY = 0;
+
+#if UNITY_STANDALONE_WIN
+   
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    private static extern bool SetWindowPos(IntPtr hwnd, int hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+   
+    [DllImport("user32.dll", EntryPoint = "FindWindow")]
+    public static extern IntPtr FindWindow(string className, string windowName);
+   
+    public static IEnumerator SetWindowPosition(int x, int y) {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        SetWindowPos(FindWindow(null, Application.productName), 0, x, y, 0, 0, 5);
+    }
+   
+#endif
+
+    void Start()
+    {
+        monitorWidth = Screen.resolutions[Screen.resolutions.Length - 1].width;
+        monitorHeight = Screen.resolutions[Screen.resolutions.Length - 1].height;
+        CheckAndSet();
+    }
+
+    private void CheckAndSet()
+    {
+        if (PlayerPrefs.GetInt(fullScreenKey, 0) >= 1)
+        {
+            SetFullScreen();
+        }
+        else
+        {
+            SetWindowed();
+        }
+    }
+
+    public void SetFullScreen()
+    {
+        Screen.SetResolution(monitorWidth, monitorHeight, true);
+        fullScreen = true;
+        PlayerPrefs.SetInt(fullScreenKey, 1);
+        PlayerPrefs.Save();
+    }
+
+    public void SetWindowed()
+    {
+        SetWindowResolution();
+        fullScreen = false;
+        PlayerPrefs.SetInt(fullScreenKey, 0);
+        PlayerPrefs.Save();
+    }
+
+    private void SetWindowResolution()
+    {
+        int multiplier = 1;
+        if (monitorWidth >= monitorHeight)
+        {
+            multiplier = monitorHeight / minDisplaySize;
+            if ((monitorHeight % minDisplaySize) == 0)
+            {
+                multiplier--;
+            }
+        }
+        else
+        {
+            multiplier = monitorWidth / minDisplaySize;
+            if ((monitorWidth % minDisplaySize) == 0)
+            {
+                multiplier--;
+            }
+        }
+        int size = minDisplaySize * multiplier;
+        if (size < minDisplaySize)
+        {
+            size = minDisplaySize;
+        }
+        Screen.SetResolution(size, size, false);
+
+#if UNITY_STANDALONE_WIN
+       
+        //int x = monitorWidth / 2;
+        //x -= size / 2;
+        //int y = monitorHeight / 2;
+        //y -= size / 2;
+        //StartCoroutine(SetWindowPosition(x, y));
+        StartCoroutine(SetWindowPosition(windowPositionX, windowPositionY));
+       
+#endif
+    }
+}
