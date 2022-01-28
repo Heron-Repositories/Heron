97a3554a65d08cc9546f5b412a8bd309482b3366
georgedimitriadis <gdimitri@hotmail.com>
2022-01-28 17:40:43 +0000

    TL_Task2_Screens_Unity Unity project with first working pub sub communication

--- Heron/Operations/Sinks/Transfer_Learning/TL_Task2_Screens_Unity/__Unity_TL_Task2_Screens_Project/Assets/Scripts/PubSub/Listener.cs
+++ Heron/Operations/Sinks/Transfer_Learning/TL_Task2_Screens_Unity/__Unity_TL_Task2_Screens_Project/Assets/Scripts/PubSub/Listener.cs
@@ -3,69 +3,67 @@ using System.Collections.Concurrent;
 using System.Threading;
 using NetMQ;
 using NetMQ.Sockets;
+using System.Text;
 
-namespace PubSub
+public class Listener
 {
-    public class Listener
+    private Thread _clientThread;
+    private readonly string _host;
+    private readonly string _port;
+    private readonly Action<string> _messageCallback;
+    private bool _clientCancelled;
+
+    private readonly ConcurrentQueue<string> _messageQueue = new ConcurrentQueue<string>();
+
+    public Listener(string host, string port, Action<string> messageCallback)
     {
-        private Thread _clientThread;
-        private readonly string _host;
-        private readonly string _port;
-        private readonly Action<string> _messageCallback;
-        private bool _clientCancelled;
+        _host = host;
+        _port = port;
+        _messageCallback = messageCallback;
+    }
 
-        private readonly ConcurrentQueue<string> _messageQueue = new ConcurrentQueue<string>();
+    public void Start()
+    {
+        _clientCancelled = false;
+        _clientThread = new Thread(ListenerWork);
+        _clientThread.Start();
+        EventManager.Instance.onClientStarted.Invoke();
+    }
 
-        public Listener(string host, string port, Action<string> messageCallback)
+    public void Stop()
+    {
+        _clientCancelled = true;
+        _clientThread?.Join();
+        _clientThread = null;
+        EventManager.Instance.onClientStopped.Invoke();
+    }
+
+    private void ListenerWork()
+    {
+        AsyncIO.ForceDotNet.Force();
+        using (var subSocket = new SubscriberSocket())
         {
-            _host = host;
-            _port = port;
-            _messageCallback = messageCallback;
-        }
-
-        public void Start()
-        {
-            _clientCancelled = false;
-            _clientThread = new Thread(ListenerWork);
-            _clientThread.Start();
-            EventManager.Instance.onClientStarted.Invoke();
-        }
-
-        public void Stop()
-        {
-            _clientCancelled = true;
-            _clientThread?.Join();
-            _clientThread = null;
-            EventManager.Instance.onClientStopped.Invoke();
-        }
-
-        private void ListenerWork()
-        {
-            AsyncIO.ForceDotNet.Force();
-            using (var subSocket = new SubscriberSocket())
+            subSocket.Options.ReceiveHighWatermark = 1000;
+            subSocket.Connect($"tcp://{_host}:{_port}");
+            subSocket.SubscribeToAnyTopic();
+            while (!_clientCancelled)
             {
-                subSocket.Options.ReceiveHighWatermark = 1000;
-                subSocket.Connect($"tcp://{_host}:{_port}");
-                subSocket.SubscribeToAnyTopic();
-                while (!_clientCancelled)
-                {
-                    if (!subSocket.TryReceiveFrameString(out var message)) continue;
-                    _messageQueue.Enqueue(message);
-                }
-                subSocket.Close();
+                if (!subSocket.TryReceiveFrameString(Encoding.ASCII, out var message)) continue;
+                _messageQueue.Enqueue(message);
             }
-            NetMQConfig.Cleanup();
+            subSocket.Close();
         }
+        NetMQConfig.Cleanup();
+    }
 
-        public void DigestMessage()
+    public void DigestMessage()
+    {
+        while (!_messageQueue.IsEmpty)
         {
-            while (!_messageQueue.IsEmpty)
-            {
-                if (_messageQueue.TryDequeue(out var message))
-                    _messageCallback(message);
-                else
-                    break;
-            }
+            if (_messageQueue.TryDequeue(out var message))
+                _messageCallback(message);
+            else
+                break;
         }
     }
 }
