fb918f710c5e9f653156c16beed6849499dd8000
georgedimitriadis <gdimitri@hotmail.com>
2022-02-01 10:43:50 +0000

    Added Spritecontroller in TL_Task2_Screens_Unity Unity project

---
+++ Heron/Operations/Sinks/Transfer_Learning/TL_Task2_Screens_Unity/__Unity_TL_Task2_Screens_Project/Assets/Scripts/Logic/SpriteController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController: MonoBehaviour
+{
+    private bool movementType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+
+        EventManager.Instance.onUpdatedMotion.AddListener(DoMotion);
+        EventManager.Instance.onUpdateMovementType.AddListener(DoMotionTypeSelection);
+        EventManager.Instance.onUpdateScreensOn.AddListener(DoScreensSelection);
+
+        movementType = true; // That means rotation
+    }
+
+    void DoMotionTypeSelection(string _movementType)
+    {
+        if (_movementType == "0")
+        {
+            movementType = true;
+        }
+        else
+        {
+            movementType = false;
+        }
+    }
+    
+    void DoScreensSelection(string screens)
+    {
+        if(transform.name.Contains("Left") && (screens=="Both" || screens== "Left"))
+        {
+            this.gameObject.SetActive(true);
+        }
+        if (transform.name.Contains("Right") && (screens == "Both" || screens == "Right"))
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+
+    void DoMotion(string coordinates)
+    {
+        int motion;
+        motion = int.Parse(coordinates);
+        if (movementType)
+            transform.rotation = Quaternion.Euler(Vector3.forward * motion);
+        else
+            transform.Translate(new Vector3((-1200 / 90 * motion) + 50, transform.position.y, transform.position.z));
+
+    }
+}
