752f909b7212815c544ba995a5cdc1155ed9eddb
georgedimitriadis <gdimitri@hotmail.com>
2022-03-04 17:02:06 +0000

    Added the Save_Pandas_DF Node

---
+++ Heron/Operations/Sinks/Saving/Save_Pandas_DF/save_pandas_df_com.py
@@ -0,0 +1,48 @@
+# This is the code that defines the Parameters, Inputs and Outputs of the Node. Most of the code should not be
+# changed. The only parts that should be changed are the information in the lists of the properties of the Node and the
+# name of the xxx_com variable under the if __name__ == "__main__": clause
+
+
+# <editor-fold desc="The following 9 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import os
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+
+from Heron import general_utils as gu
+
+Exec = os.path.realpath(__file__)
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called from the GUI process as part of the generation of the node.
+# It is meant to create Node specific elements.
+# This is where a new Node's individual elements should be defined">
+
+"""
+Properties of the generated Node
+"""
+BaseName = 'Save Pandas DF'  # The base name can have spaces.
+
+NodeAttributeNames = ['Parameters', 'Row In']
+NodeAttributeType = ['Static', 'Input']
+ParameterNames = ['Visualisation', 'Comma Delimited Column Names', 'File Name', 'Overwrite']
+ParameterTypes = ['bool', 'str', 'str', 'bool']
+ParametersDefaultValues = [False, 'Column 1, Column 2', r'C:\test.df', False]
+WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'save_pandas_df_worker.py')
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called as its own process when the editor starts the graph.
+#  You can refactor the name of the xxx_com variable but do not change anything else">
+if __name__ == "__main__":
+    #  In this case refactor the name sink_template_com to whatever_com
+    save_pandas_df_com = gu.start_the_sink_communications_process()
+    gu.register_exit_signals(save_pandas_df_com.on_kill)
+    save_pandas_df_com.start_ioloop()
+# </editor-fold>
---
+++ Heron/Operations/Sinks/Saving/Save_Pandas_DF/save_pandas_df_worker.py
@@ -0,0 +1,101 @@
+
+# <editor-fold desc="The following 6 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+# </editor-fold>
+
+# <editor-fold desc="Extra imports if required">
+import os
+import pandas as pd
+from datetime import datetime
+from Heron.communication.socket_for_serialization import Socket
+from Heron.gui.visualisation import Visualisation
+from Heron import general_utils as gu
+# </editor-fold>
+
+column_names_str: str
+file_name: str
+overwrite_file: bool
+vis: Visualisation
+df: pd.DataFrame
+# </editor-fold>
+
+
+def initialise(_worker_object):
+    global vis
+    global column_names_str
+    global file_name
+    global overwrite_file
+    global df
+
+    # put the initialisation of the Node's parameter's in a try loop to take care of the time it takes for the GUI to
+    # update the SinkWorker object.
+    try:
+        parameters = _worker_object.parameters
+        vis = parameters[0]
+        column_names_str = parameters[1]
+        file_name = parameters[2]
+        overwrite_file = parameters[3]
+    except:
+        return False
+
+    # Ignore visualisation for now
+    #vis = Visualisation(_worker_object.node_name, _worker_object.node_index)
+    #vis.visualisation_init()
+
+    if os.path.isfile(file_name):
+        if overwrite_file:
+            os.remove(file_name)
+        else:
+            print('File {} exists and cannot be overwritten so data will not be saved'.format(file_name))
+
+    column_names = column_names_str.split(',')
+
+    df = pd.DataFrame(columns=column_names)
+    return True
+
+
+def work_function(data, parameters):
+    global vis
+    global df
+
+
+    try:
+        vis.visualisation_on = parameters[0]
+    except:
+        pass
+
+
+    topic = data[0]
+
+    message = data[1:]
+    message = Socket.reconstruct_array_from_bytes_message(message)
+    #print('--- Message in Save DF: {}'.format(message))
+    time = datetime.now()
+    row = pd.DataFrame([message], columns=df.columns, index=[time])
+    #print('--- DF Row: {}'.format(row))
+    df = pd.concat([df, row], ignore_index=False)
+    #vis.visualised_data = np.random.random((100,100))
+
+
+def on_end_of_life():
+    global df
+    global file_name
+
+    if not os.path.isfile(file_name):
+        df.to_pickle(file_name)
+
+
+# This needs to exist. The worker_function and the end_of_life function must be defined and passed. The initialisation_
+# function is optional.
+if __name__ == "__main__":
+    worker_object = gu.start_the_sink_worker_process(work_function=work_function,
+                                                     end_of_life_function=on_end_of_life,
+                                                     initialisation_function=initialise)
+    worker_object.start_ioloop()
