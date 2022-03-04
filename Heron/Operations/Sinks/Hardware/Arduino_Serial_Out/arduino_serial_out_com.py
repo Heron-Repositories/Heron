c51106a2da8f2a0e5f5433cbb1fe867941d1e519
georgedimitriadis <gdimitri@hotmail.com>
2022-03-04 17:01:16 +0000

    Added the Arduino_Serial_Out Node

---
+++ Heron/Operations/Sinks/Hardware/Arduino_Serial_Out/arduino_serial_out_com.py
@@ -0,0 +1,41 @@
+
+# <editor-fold desc="The following 9 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import os
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+
+from Heron import general_utils as gu
+
+Exec = os.path.realpath(__file__)
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called from the GUI process as part of the generation of the node.
+"""
+Properties of the generated Node
+"""
+BaseName = 'Arduino Serial Out'  # The base name can have spaces.
+
+NodeAttributeNames = ['Parameters', 'Signal In']
+NodeAttributeType = ['Static', 'Input']
+ParameterNames = ['Visualisation', 'COM Port', 'Baud Rate']
+ParameterTypes = ['bool', 'str', 'int']
+ParametersDefaultValues = [False, 'COM4', 9600]
+WorkerDefaultExecutable = os.path.join(os.path.dirname(Exec), 'arduino_serial_out_worker.py')
+# </editor-fold>
+
+
+# <editor-fold desc="The following code is called as its own process when the editor starts the graph.
+#  You can refactor the name of the xxx_com variable but do not change anything else">
+if __name__ == "__main__":
+    #  In this case refactor the name sink_template_com to whatever_com
+    arduino_serial_out_com = gu.start_the_sink_communications_process()
+    gu.register_exit_signals(arduino_serial_out_com.on_kill)
+    arduino_serial_out_com.start_ioloop()
+# </editor-fold>
---
+++ Heron/Operations/Sinks/Hardware/Arduino_Serial_Out/arduino_serial_out_worker.py
@@ -0,0 +1,84 @@
+
+# <editor-fold desc="The following 6 lines of code are required to allow Heron to be able to see the Operation without
+# package installation. Do not change.">
+import sys
+from os import path
+
+current_dir = path.dirname(path.abspath(__file__))
+while path.split(current_dir)[-1] != r'Heron':
+    current_dir = path.dirname(current_dir)
+sys.path.insert(0, path.dirname(current_dir))
+# </editor-fold>
+
+# <editor-fold desc="Extra imports if required">
+import serial
+from Heron.communication.socket_for_serialization import Socket
+from Heron import general_utils as gu
+# </editor-fold>
+
+# <editor-fold desc="Global variables if required. Global variables operate obviously within the scope of the process
+# that is running when this script is called so they pose no existential threats and are very useful in keeping state
+# over different calls of the work function (see below).">
+com_port: str
+baud_rate: int
+vis: bool
+
+arduino_serial: serial.Serial
+
+# </editor-fold>
+
+
+def initialise(_worker_object):
+    global arduino_serial
+    global vis
+
+    try:
+        parameters = _worker_object.parameters
+        vis = parameters[0]
+        com_port = parameters[1]
+        baud_rate = parameters[2]
+
+    except:
+        return False
+
+    try:
+        arduino_serial = serial.Serial(port=com_port, baudrate=baud_rate)
+    except Exception as e:
+        print(e)
+
+    return True
+
+
+def work_function(data, parameters):
+    global vis
+    global arduino_serial
+
+    try:
+        vis = parameters[0]
+    except:
+        pass
+
+    topic = data[0]
+
+    message = data[1:]
+    message = Socket.reconstruct_array_from_bytes_message(message)[0]
+
+    if vis:
+        print(message)
+
+    arduino_serial.write(message.encode('utf-8'))
+
+
+def on_end_of_life():
+    global arduino_serial
+    arduino_serial.reset_input_buffer()
+    arduino_serial.close()
+
+
+# This needs to exist. The worker_function and the end_of_life function must be defined and passed. The initialisation_
+# function is optional.
+if __name__ == "__main__":
+    worker_object = gu.start_the_sink_worker_process(work_function=work_function,
+                                                     end_of_life_function=on_end_of_life,
+                                                     initialisation_function=initialise)
+    worker_object.start_ioloop()
