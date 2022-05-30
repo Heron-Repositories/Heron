9ff0ebad5880f3f881f8033ab93a170a4b456eac
georgedimitriadis <gdimitri@hotmail.com>
2022-05-30 23:59:03 +0100

    The setup file for pip

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='Heron',
+    version='0.6.0',
+    description='A python framework for experimental pipelines',
+    url='https://github.com/Heron-Repositories/Heron',
+    author='George Dimitriadis',
+    author_email = 'gdimitri@hotmail.com',
+    license='MIT',
+    keywords=['Scientific', 'Cluster', 'Experiments'],
+    packages=['Heron'],
+    install_requires=['pyzmq>=20.x',
+                      'numpy',
+                      'dearpygui>=1.2',
+                      'paramiko',
+                      'pandas',
+                      'h5py',
+                      'tornado',
+                      'pynput',
+                      'serial'
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
