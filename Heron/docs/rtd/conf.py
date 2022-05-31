62bde74209727908488b50d9bc43c134684db941
georgedimitriadis <gdimitri@hotmail.com>
2022-05-31 21:39:35 +0100

    more docs changes

--- Heron/docs/rtd/conf.py
+++ Heron/docs/rtd/conf.py
@@ -30,7 +30,6 @@ author = 'George Dimitriadis'
 extensions = [
 ]
 
-extensions = ['sphinx.ext.autodoc']
 autodoc_typehints = 'description'
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,4 +51,3 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
