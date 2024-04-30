4e8a52d4b0e3abcc4c60961b34ddf41460c50b79
georgedimitriadis <gdimitri@hotmail.com>
2024-04-30 07:04:00 +0100

    The Browse buttons in the settings GUI now work

--- Heron/gui/settings.py
+++ Heron/gui/settings.py
@@ -5,6 +5,8 @@ import os
 from pathlib import Path
 import json
 
+from Heron.gui.fdialog import FileDialog
+
 heron_path = Path(os.path.dirname(os.path.realpath(__file__))).parent
 settings_file = join(heron_path, 'settings.json')
 if not os.path.isfile(settings_file):
@@ -47,6 +49,27 @@ def on_close_main_with_buttons(sender, app_data, user_data):
     dpg.delete_item(node_win)
 
 
+def on_browse(sender, app_data, user_data):
+    input_txt = user_data[0]
+    dirs_only = user_data[1]
+    setting_keys = user_data[2]
+
+    def on_path_selected(files_selected):
+        file = files_selected[0]
+        dpg.set_value(input_txt, file)
+        if len(setting_keys) == 2:
+            settings_dict[setting_keys[0]][setting_keys[1]] = dpg.get_value(input_txt)
+        elif len(setting_keys) == 3:
+            settings_dict[setting_keys[0]][setting_keys[1]][setting_keys[2]] = dpg.get_value(input_txt)
+        save_settings()
+
+    file_dialog = FileDialog(title=f'Select {"folder" if dirs_only else "folder"}', show_dir_size=False,
+                             modal=False, allow_drag=False, show_hidden_files=True, multi_selection=False,
+                             tag='file_dialog',  default_path=os.path.expanduser('~'), dirs_only=dirs_only,
+                             callback=on_path_selected)
+    file_dialog.show_file_dialog()
+
+
 def start():
     side_windows = []
 
@@ -79,13 +102,18 @@ def start():
 
             with dpg.child_window(label='IDE', parent=settings_group, pos=[200, 30], show=False) as ide_window:
                 dpg.add_spacer(height=30)
-                dpg.add_input_text(label='IDE executable', default_value=settings_dict['IDE']['IDE Path'],
-                                   callback=update_setting, user_data=['IDE', 'IDE Path'], on_enter=True)
-                dpg.add_button(label='Browse')
+                ide_exe_input = dpg.add_input_text(label='IDE executable',
+                                                   default_value=settings_dict['IDE']['IDE Path'],
+                                                   callback=update_setting, user_data=['IDE', 'IDE Path'],
+                                                   on_enter=True)
+                dpg.add_button(label='Browse', user_data=[ide_exe_input, False, ['IDE', 'IDE Path']], callback=on_browse)
                 dpg.add_spacer(height=30)
-                dpg.add_input_text(label='Project', default_value=settings_dict['IDE']['IDE Project Path'],
-                                   callback=update_setting, user_data=['IDE', 'IDE Project Path'], on_enter=True)
-                dpg.add_button(label='Browse')
+                ide_project_input = dpg.add_input_text(label='Project',
+                                                       default_value=settings_dict['IDE']['IDE Project Path'],
+                                                       callback=update_setting, user_data=['IDE', 'IDE Project Path'],
+                                                       on_enter=True)
+                dpg.add_button(label='Browse', user_data=[ide_project_input, True, ['IDE', 'IDE Project Path']],
+                               callback=on_browse)
             side_windows.append(ide_window)
 
             with dpg.child_window(label='Operation', parent=settings_group, pos=[200, 30], show=False) as operation_window:
@@ -114,9 +142,12 @@ def start():
                                   width=140, callback=update_setting, user_data=['Operation', 'DELAY_BETWEEN_SENDING_DATA_TO_NEXT_NODE_MILLISECONDS'],
                                   on_enter=True)
                 dpg.add_spacer(height=20)
-                dpg.add_input_text(label='KNOWN_HOSTS_FILE', default_value=settings_dict['Operation']['KNOWN_HOSTS_FILE'],
-                                   callback=update_setting, user_data=['Operation', 'KNOWN_HOSTS_FILE'], on_enter=True)
-                dpg.add_button(label='Browse')
+                khf_input = dpg.add_input_text(label='KNOWN_HOSTS_FILE',
+                                               default_value=settings_dict['Operation']['KNOWN_HOSTS_FILE'],
+                                               callback=update_setting, user_data=['Operation', 'KNOWN_HOSTS_FILE'],
+                                               on_enter=True)
+                dpg.add_button(label='Browse', user_data=[khf_input, False, ['Operation', 'KNOWN_HOSTS_FILE']],
+                               callback=on_browse)
             side_windows.append(operation_window)
 
             with dpg.child_window(parent=setting_window, width=190, pos=[5, 30]) as options_window:
